6bb6738a9e63ae7091f5972159627aeaef079258
saad7mahmud <saad7mahmud@gmail.com>
2023-10-21 17:21:10 +0600

    delete system updated

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ async function run() {
     app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
       console.log("delete from db", id);
-      const query = { _id: id };
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
