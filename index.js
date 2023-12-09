8e4bad54b3c42f609e5814a8b48375eb9e573c07
saad7mahmud <saad7mahmud@gmail.com>
2023-12-09 06:36:02 +0600

    uri secret

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
+
 const uri =
-  "mongodb+srv://brand-shop-database:Rw0v6mZqd11HFmuI@cluster0.znptc55.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znptc55.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -45,7 +46,6 @@ async function run() {
 
     //   read/get from cart
     app.get("/cart", async (req, res) => {
-     
       const result = await cartCollection.find({}).toArray();
       res.send(result);
     });
@@ -112,7 +112,6 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
   }
 }
 run().catch(console.dir);
